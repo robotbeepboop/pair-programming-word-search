ac94500ed843e5d2efe385b73898c140121d3881
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-04-12 14:27:34 -0700

    all tests pass

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,33 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+//pair programming with Matthew Charm, Ciara Biddiscombe, Kamile Lemaire
 
-module.exports = wordSearch
+const transpose = function (matrix) {
+ 
+  let array = [];
+
+  for (let i = 0; i < matrix[0].length; i++) {
+      array.push([]);
+  };
+
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = 0; c < matrix[r].length; c++) {
+      array[c][r] = matrix[r][c];
+    }
+  }
+  return array;
+};
+
+const wordSearch = (letters, word) => { 
+    let horizontalJoin = letters.map(ls => ls.join('')); //use .map for vert as well
+    for (let x of horizontalJoin) {
+        if (x.includes(word)) return true;
+    }
+    let verticalJoin = transpose(letters).map(y => y.join('')); //can use transpose instead ??
+    for (let y of verticalJoin) {
+        if (y.includes(word)) return true;
+    }
+
+    return false;
+};
+
+
+module.exports = wordSearch;
